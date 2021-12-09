2c16b6463b4d434db2db6860c92a226b5610b59f
haleywitherell <hwitherell22@gmail.com>
2021-12-08 20:48:34 -0500

    updates

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -3,6 +3,7 @@ const FILES_TO_CACHE = [
     "/icons/icon-192x192.pgn",
     "/icons/icon-512x512.pgn",
     "/index.html",
-
+    "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+    "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ];
 
